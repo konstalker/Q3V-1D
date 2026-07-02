ea43aa692fa143ba2bbc59e649ced931caee72bd
KONSTALKER <konstalker@bk.ru>
2026-07-02 11:49:48 +0300

    added engine folder

---
+++ engines/launch.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: =====================================================
+:: Укажите полные пути к приложениям или имена файлов,
+:: если они находятся в той же папке, что и bat-файл.
+:: =====================================================
+set VULKAN_APP="oDFe.vk.x64.exe"
+set FALLBACK_APP="oDFe.x64.exe"
+
+set ARGS=+set fs_homepath "%%~fI\baseq3\mods" +set fs_basepath "%%~fI" +set fs_game "osp"
+
+:: Проверка наличия Vulkan API (системной библиотеки)
+set VULKAN_PRESENT=0
+if exist "%SystemRoot%\System32\vulkan-1.dll" set VULKAN_PRESENT=1
+if exist "%SystemRoot%\SysWOW64\vulkan-1.dll" set VULKAN_PRESENT=1
+
+:: Запуск
+for %%I in ("%~dp0..") do (
+    if %VULKAN_PRESENT% equ 1 (
+        echo Vulkan API detected. Launching Vulkan application...
+        echo "" %VULKAN_APP% %ARGS%
+        start "" %VULKAN_APP% %ARGS%
+    ) else (
+        echo Vulkan API not detected. Launching fallback application...
+        echo "" %FALLBACK_APP% %ARGS%
+        start "" %FALLBACK_APP% %ARGS%
+    )
+)
+
+endlocal
