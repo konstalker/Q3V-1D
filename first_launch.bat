7837b43cd541ca3052819d53356111a6cb7b716d
KONSTALKER <konstalker@bk.ru>
2026-07-02 11:49:19 +0300

    Create first_launch.bat

---
+++ first_launch.bat
@@ -0,0 +1,7 @@
+@echo off
+set "shortcutName=Quake 3 Arena.lnk"
+set "targetPath=%~dp0engines\launch.bat"
+set "iconPath=%~dp0engines\icons\b3.ico"
+set "workingDir=%~dp0engines"
+
+powershell -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut(\"%shortcutName%\"); $s.TargetPath = \"%targetPath%\"; $s.IconLocation = \"%iconPath%\"; $s.WorkingDirectory = \"%workingDir%\"; $s.Save()"
