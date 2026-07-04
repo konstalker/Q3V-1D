4ca1ca93e4f111483aa873454dc93c380d27787e
KONSTALKER <konstalker@bk.ru>
2026-07-04 12:18:12 +0300

    Update first_launch.bat

--- first_launch.bat
+++ first_launch.bat
@@ -5,6 +5,7 @@ cd /d %~dp0/engines
 call downloader.bat "./pack_list/files.txt" "f" "s"
 call downloader.bat "./pack_list/archives.txt" "a" "s"
 call ./beupd.bat
+call ./updater.bat
 
 cd /d %~dp0
 
