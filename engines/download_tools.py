44c4e5cf12a0390879e1c5880eaa3ced0cc7999b
konstalker <konstalker@bk.ru>
2026-07-05 13:47:56 +0300

    changes

--- engines/download_tools.py
+++ engines/download_tools.py
@@ -1,38 +1,70 @@
+from ast import Delete
 import urllib.request
+from multiprocessing import Process
 from sys import argv
 import os
 from math import floor
-import zipfile
+import time
 
+def __download_proc(file_url, file_path, skip, index, processes, total_length, chunk_size):
 
-def download(file_url, file_path, file_name, skip=False):
-
-    percent = 0
+    downloaded = index * (total_length // processes) + os.path.getsize(file_path + file_name + f'part{index}')
+    if processes - 1 == index:
+        partend = ''
+    else:
+        partend = (index + 1) * floor(total_length / processes)
     
+    headers = {'Range': f'bytes={downloaded}-{partend}'}
+    req = urllib.request.Request(file_url, headers=headers)
+
+    with urllib.request.urlopen(req) as response, open(file_path + file_name + f'.part{index}', 'ab') as out_file:
+        while True:
+            chunk = response.read(chunk_size)
+            if not chunk:
+                break 
+            
+            out_file.write(chunk)
+    
+
+def download(file_url, file_path, file_name, skip=False, processes=1, chunk_size=8192):
     with urllib.request.urlopen(file_url) as response:
         
         total_length = response.info().get('Content-Length')
+        total_length = int(total_length)
         
         print(f"Installing {file_name}")
-        total_length = int(total_length)
         
         if skip and os.path.exists(file_path + file_name) and os.path.getsize(file_path + file_name) == total_length:
             print("File exists, skipping.")
             return
-        
+
+        downloaders = []
         downloaded = 0
 
-        headers = {}
+        last_percent = 0
+        percent = 0
         
-        if skip and os.path.exists(file_path + file_name):
-            downloaded = os.path.getsize(file_path + file_name)
-            headers = {'Range': f'bytes={downloaded}-'}
-        
-        chunk_size = 8192
+        for i in range(processes):
+            downloaders.append(Process(target=__download_proc, args=(file_url, file_path, skip, i, processes, total_length, chunk_size)))
 
-    req = urllib.request.Request(file_url, headers=headers)
+        while all([downloaders[i].is_alive() for i in range(processes)]):
+            for i in range(processes):
+                if os.path.exists(file_path + file_name + f'part{i}'):
+                    downloaded += os.path.getsize(file_path + file_name + f'part{i}')
+                    
+            last_percent = percent
+            percent = int((downloaded / total_length) * 100)
+            if last_percent != percent:
+                print(f"\r[{"#" * floor(percent / 5)}{' ' * ((100 - percent) // 5)}] {percent}% ({total_length // 1048576} MB)   ", end='')
+            time.sleep(0.5)
+
+        Delete(downloaders)
+        
+    print("\ninstalled")
+
+        
     
-    with urllib.request.urlopen(req) as response, open(file_path + file_name, 'ab') as out_file:
+    '''with urllib.request.urlopen(req) as response, open(file_path + file_name, 'ab') as out_file:
     
         print("downloading...")
         while True:
@@ -48,7 +80,7 @@ def download(file_url, file_path, file_name, skip=False):
             percent = int((downloaded / total_length) * 100)
 
             if last_percent != percent:
-                print(f"\r[{"#" * floor(percent / 5)}{' ' * ((100 - percent) // 5)}] {percent}% ({total_length // 1048576} MB)   ", end='')
+                print(f"\r[{"#" * floor(percent / 5)}{' ' * ((100 - percent) // 5)}] {percent}% ({total_length // 1048576} MB)   ", end='')'''
     
     print("\ninstalled.")
 
--- engines/installer.py
+++ engines/installer.py
@@ -4,8 +4,10 @@ import download_tools as dt
 with open("./pack_files/files.xml", 'r') as pack_file:
     pack_list = pack_file.read().split('\n')
     for _ in pack_list:
-        url, path, name = _.split(';')
+        if ";" in _:
 
-        dt.download(url, path, name, skip=True)
+            url, path, name = _.split(';')
+            
+            dt.download(url, path, name, skip=True, processes=1)
 
-print("installed.")
+print("All packs installed.")
