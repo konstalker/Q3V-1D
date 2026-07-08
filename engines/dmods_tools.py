2cd0578c30541310045942d252418cba57ee41a6
KONSTALKER <konstalker@bk.ru>
2026-07-08 11:56:45 +0300

    Update dmods_tools.py

--- engines/dmods_tools.py
+++ engines/dmods_tools.py
@@ -4,5 +4,6 @@ import os
 
 
 class dmod:
-    def __init__(self, mod_branch):
-        pass
+    def __init__(self, mod_name):
+        with open(f"./mod_tree/{mod_name}.dmod") as mod_branch:
+            
