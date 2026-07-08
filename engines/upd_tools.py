225cba50bd1d887181d951b9b0037c703882b31d
KONSTALKER <konstalker@bk.ru>
2026-07-08 16:14:26 +0300

    deleted unused code (mod installer vorks correctly)

--- engines/upd_tools.py
+++ engines/upd_tools.py
@@ -56,48 +56,5 @@ def __install(repo_name, compilation_branch, mod_branch, local_name=''):
         caption()
 
 
-
-# def update_be():
-
-#     if not os.path.exists("./temp_files"):
-#         os.mkdir("./temp_files")
-    
-#     name = "zz-osp-pak8be.pk3"
-#     be_url = "https://konstalker.github.io/assets/zz-osp-pak8be.pk3"
-    
-#     archive_path = '../baseq3/mods/osp/'
-#     vurl = "https://konstalker.github.io/assets/version.txt"
-#     vfile = 'version.txt'
-
-#     dt.downloader(vurl, "./temp_files/", vfile)
-#     download = False
-    
-#     try:
-#         if not os.path.exists(archive_path + name):
-#             download = True
-        
-#         if not download:
-#             with zipfile.ZipFile(archive_path + name, 'r') as archive:
-#                 with archive.open(vfile) as oldfile, open(f"./temp_files/{vfile}", 'r') as newfile:
-#                     old = oldfile.read().decode('utf-8')
-#                     new = newfile.read()
-#                     if old < new:
-#                         download = True
-#                         os.remove(archive_path + name)
-
-#         if download:
-#             dt.downloader(be_url, archive_path, name, skip=True)
-#         else:
-#             print(f"[log] you have newest version of {name}")
-
-#         shutil.rmtree("./temp_files/")
-                
-#     except Exception as e:
-#         print("[error] new versoin of OSP2-BE not installed")
-#         print(f"[log] error: {e}")
-#         caption()
-
-
 if __name__ == "__main__":
     update("osp2-be")
-    update("oDFe")
