7af1c436f78fc314d62fb909f3518b7e997c7cdf
konstalker <konstalker@bk.ru>
2026-07-09 09:08:44 +0300

    updates

--- engines/flaunch.py
+++ engines/flaunch.py
@@ -1,8 +1,9 @@
 import download_tools as dt
 from autoupdater import autoupdate
 
-with open('./mod_tree/branch.txt', 'r') as bfile, open('../version', 'r') as vfile:
+with open('./mod_tree/branch.txt', 'r') as bfile, open('../version', 'r') as vfile, open('../sversion', 'r') as svfile:
     text = bfile.read().replace('@', vfile.read().rstrip())
+    text = bfile.read().replace('$', svfile.read().rstrip())
 
 with open('./mod_tree/branch.txt', 'w') as bfile:
     bfile.write(text)
