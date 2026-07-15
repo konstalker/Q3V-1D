e3c9cfaf1355561d68f5e9ed4c9054fc5e97fa89
KONSTALKER <118746501+konstalker@users.noreply.github.com>
2026-07-15 21:50:45 +0300

    Update upd_tools.py

--- engines/upd_tools.py
+++ engines/upd_tools.py
@@ -9,6 +9,24 @@ from base_methods import caption, furl, c_info
 from dmods_tools import dmod_conf
 
 
+def autoupdate():
+    if not os.path.exists("./temp_files"):
+        os.mkdir("./temp_files")
+
+    dt.downloader(furl('[RURL]index.modlist'), "./temp_files/", "modlist.txt", skip=True)
+   
+    with open("./temp_files/modlist.txt", 'r') as f:
+        for x in dmod_conf.mod_list():
+            update(x)
+
+    dmod_conf.save()
+
+    try:
+        shutil.rmtree("./temp_files/")
+    except Exception:
+        pass
+
+
 def update(repo_name):
     try:
         if not os.path.exists("./temp_files"):
