52d5247c69946c59e5cbb5212127ad6236dcd7b6
konstalker <konstalker@bk.ru>
2026-07-28 21:59:14 +0300

    some changes

--- engines/bmods_tools.py
+++ engines/bmods_tools.py
@@ -51,15 +51,20 @@ class bmod:
             for i, x in enumerate(self.mod_info[tag]):
                 if x[0] == key:
                     position = i
-                    
+                    break
+            
             if position >= 0:
                 self.mod_info[tag][position][0] = key
-                self.mod_info[tag][position][1] = value
-            else:
+                if value == None:
+                    self.mod_info[key].remove(y)
+                    if not self.mod_info[key]:
+                        self.mod_info.pop(key)
+                else:
+                    self.mod_info[tag][position][1] = value
+            elif value != None:
                 self.mod_info[tag].append([key, value])
                 
         else:
-            print(tag, key, value)
             self.mod_info.update({tag: [[key, value]]})
     
     def mod_list(self):
