46ac47b1a2d713cc5fe48109e9bedffa160b1061
KONSTALKER <konstalker@bk.ru>
2026-08-13 15:04:15 +0300

    Update setup_python.bat

--- engines/python/setup_python.bat
+++ engines/python/setup_python.bat
@@ -14,6 +14,7 @@ set "VENV_DIR=%PY_ROOT%\venv"
 set "REQUIREMENTS=%BASE_DIR%requirements.txt"
 set "PYTHON_VERSION=3.12"
 set "UV_ZIP_URL=https://github.com/astral-sh/uv/releases/latest/download/uv-x86_64-pc-windows-msvc.zip"
+chcp 65001 >nul
 
 :: === Полная изоляция: перенаправляем ВСЕ рабочие каталоги uv внутрь PY_ROOT ===
 set "UV_CACHE_DIR=%PY_ROOT%\cache"
